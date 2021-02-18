ffc2eeff1093fde5a16eaa5fb446daabd089ec44
Oskar Yildiz <yildizoskar@gmail.com>
2021-02-18 19:17:17 +0100

    created home screen

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Layout, Text } from '@ui-kitten/components'
+
+const HomeScreen = () => {
+  return (
+    <Layout style={{flex: 1}}>
+      <Text category="h2">
+        Översikt
+      </Text>
+    </Layout>
+  )
+}
+
+export default HomeScreen
