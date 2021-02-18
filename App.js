cb45c050ba216ffedbc066bfd147b81cb1715f5e
Lazurq <77831328+Lazurq@users.noreply.github.com>
2021-02-18 21:13:42 +0100

    Update App.js
    added theme changing functionality

--- App.js
+++ App.js
@@ -1,30 +1,43 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import * as eva from "@eva-design/eva";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { LoginStack } from "./src/navigation";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { default as theme } from "./config/custom-theme.json";
-import * as eva from "@eva-design/eva";
+import { ThemeContext } from './config/ThemeContext';
 
 export default function App() {
+  const [theme, setTheme] = React.useState("light");
+
+  const toggleTheme = () => {
+    const nextTheme = theme === "light" ? "dark" : "light";
+    setTheme(nextTheme);
+  };
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <LoginStack />
-        </NavigationContainer>
-      </SafeAreaView>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <ApplicationProvider {...eva} theme={eva[theme]}>
+          <SafeAreaView style={styles.container}>
+            <NavigationContainer>
+              <LoginStack />
+            </NavigationContainer>
+          </SafeAreaView>
+        </ApplicationProvider>
+      </ThemeContext.Provider>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
