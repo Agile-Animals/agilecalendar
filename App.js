e3c543b4e25f15469cd4406068cf348b2150832a
Oskar Yildiz <yildizoskar@gmail.com>
2021-02-18 19:16:48 +0100

    added SafeAreaview around everything

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import {LoginStack} from './src/navigation'
-import {ApplicationProvider } from '@ui-kitten/components';
-import { default as theme } from './config/custom-theme.json';
-import * as eva from '@eva-design/eva';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { LoginStack } from "./src/navigation";
+import { ApplicationProvider } from "@ui-kitten/components";
+import { default as theme } from "./config/custom-theme.json";
+import * as eva from "@eva-design/eva";
 
 export default function App() {
   return (
     <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-    <NavigationContainer>
-      <LoginStack />
-    </NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <LoginStack />
+        </NavigationContainer>
+      </SafeAreaView>
     </ApplicationProvider>
   );
 }
@@ -21,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
