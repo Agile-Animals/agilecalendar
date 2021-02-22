a2adfb6a1798ef9619c11475f17554451583ccf9
Lazurq <77831328+Lazurq@users.noreply.github.com>
2021-02-22 23:33:56 +0100

    Update HomeScreen.js
    Trying out dimensions to scale better, also played around with ui kitten list.

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,54 +1,80 @@
 import * as React from "react";
-import { StyleSheet, FlatList } from "react-native";
-import { Calendar, Layout, Text, Button } from "@ui-kitten/components";
+import { StyleSheet, FlatList, View, Dimensions } from "react-native";
+import {
+  Icon,
+  Layout,
+  Text,
+  Button,
+  List,
+  ListItem,
+} from "@ui-kitten/components";
+import { useForm, Controller } from "react-hook-form";
+import { ScreenOrientation } from 'expo';
 
-const DATA = [
+const data = [
   {
     id: "test 1",
     kund: "Arne",
     tid: "Måndag-10:15",
     title: "Insats 1",
+    typ: "Städa",
   },
   {
     id: "test 2",
     kund: "Kerstin",
-    tid: "Måndag-10:15",
+    tid: "Tisdag-11:15",
     title: "Insats 2",
+    typ: "Handla",
   },
   {
     id: "test 3",
     kund: "Bengt",
-    tid: "Måndag-10:15",
+    tid: "Fredag-15:15",
     title: "Insats 3",
+    typ: "Städa",
   },
   {
     id: "test 4",
     kund: "Ove",
-    tid: "Måndag-10:15",
+    tid: "Onsdag-09:15",
     title: "Insats 4",
+    typ: "Duscha",
   },
 ];
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
+
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
 
 const HomeScreen = () => {
-  const renderItem = ({ item, index }) => (
-    <ListItem title={`${item.title} ${index + 1}`} />
+  const renderItemAccessory = (props) => (
+    <Button size="tiny" onPress={handleSubmit(onSubmit)}>
+      Remove
+    </Button>
   );
+
+  const renderItemIcon = (props, { item }) => <Icon {...props} name="shopping-cart" />;
+  const renderItem = ({ item, index }) => (
+    <ListItem
+      title={`${item.title}`}
+      description={`${item.kund}`}
+      accessoryLeft={renderItemIcon}
+      accessoryRight={renderItemAccessory}
+    />
+  );
+  const { control, handleSubmit, errors } = useForm();
+  const onSubmit = (item) => {
+    console.log( `${item.title}` );
+  };
 
   return (
     <Layout style={styles.container} level="1">
       <Layout style={styles.header} level="1">
         <Text category="h2">Översikt</Text>
       </Layout>
-      <FlatList>
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      </FlatList>
+      <Button style={{ width: 140 }} onPress={handleSubmit(onSubmit)}>
+        Lägg till insats
+      </Button>
+      <List style={styles.container} data={data} renderItem={renderItem} />
     </Layout>
   );
 };
@@ -57,12 +83,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    alignItems: "center",
+    width: window.width * 1,
+    height: window.width * 1,
+    // alignItems: "center",
   },
   header: {
     alignItems: "center",
     padding: 20,
     paddingTop: 30,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
 
