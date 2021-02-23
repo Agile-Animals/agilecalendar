acaf1c24e0eec87431809d8fc5670330900d79b4
Lazurq <77831328+Lazurq@users.noreply.github.com>
2021-02-23 09:47:46 +0100

    Revert "Merge branch 'feature/loginUI'"
    This reverts commit 8bea22a03af3e4b07bd00213a68b393b016e7c25, reversing
    changes made to f4a46545511af8d256c28b83d3dea05c35bfb830.

--- App.js
+++ App.js
@@ -1,30 +1,21 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { LoginStack } from "./src/navigation";
-import { ApplicationProvider } from "@ui-kitten/components";
-import { default as theme } from "./config/custom-theme.json";
-import * as eva from "@eva-design/eva";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <NavigationContainer>
-          <LoginStack />
-        </NavigationContainer>
-      </SafeAreaView>
-    </ApplicationProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
